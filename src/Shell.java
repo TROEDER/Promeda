5daa53a8131f5ba670d0365d8ee3225deb09e8c7
TROEDER <timo.roeder@gmail.com>
2018-10-16 02:29:53 +0200

    java image compression

--- src/Shell.java
+++ src/Shell.java
@@ -1,48 +1,55 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Iterator;
+import javax.imageio.*;
+import javax.imageio.stream.*;
+import javax.swing.JFileChooser;
 
 public class Shell {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Shell obj = new Shell();
+		File files = null;
 
-		String domainName = "google.com";
-		
-		//in mac oxs
-		//String command = "ping -c 3 " + domainName;
-		
-		//in windows
-		//String command = "ping -n 3 " + domainName;
-		String command = "npm install gulp@latest";
-		String output = obj.executeCommand(command);
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogType(JFileChooser.OPEN_DIALOG);
+		fileChooser.setDialogTitle("Select files (multiple selection possible)");
+		fileChooser.setMultiSelectionEnabled(true);
+		fileChooser.setLocation(100, 100);
 
-		System.out.println(output);
+		int returnVal = fileChooser.showOpenDialog(null);
 
-	}
-
-	private String executeCommand(String command) {
-
-		StringBuffer output = new StringBuffer();
-
-		Process p;
-		try {
-			p = Runtime.getRuntime().exec(command);
-			p.waitFor();
-			BufferedReader reader = 
-                            new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-                        String line = "";			
-			while ((line = reader.readLine())!= null) {
-				output.append(line + "\n");
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			files = fileChooser.getSelectedFile();
 		}
 
-		return output.toString();
+		BufferedImage image = ImageIO.read(files);
 
+		File compressedImageFile = new File("compressed_image.jpg");
+		OutputStream os = new FileOutputStream(compressedImageFile);
+
+		Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+		ImageWriter writer = (ImageWriter) writers.next();
+
+		ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+		writer.setOutput(ios);
+
+		ImageWriteParam param = writer.getDefaultWriteParam();
+
+		
+		if (param.canWriteProgressive()) {
+			param.setProgressiveMode(ImageWriteParam.MODE_DEFAULT);
+		}
+
+		if (param.canWriteCompressed()) {
+			param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+			param.setCompressionQuality(0.7f); // Change the quality value you prefer
+		}
+
+		writer.write(null, new IIOImage(image, null, null), param);
+
+		os.close();
+		ios.close();
+		writer.dispose();
 	}
-
 }
